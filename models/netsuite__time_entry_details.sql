0d40c2fed7aedf3d8eaf0dd0ed16235360c96fd5
Evan Phillips <45015207+phillem15@users.noreply.github.com>
2023-10-10 17:20:52 -0500

    netsuite__time_entry_details features bug fix (#16)
    * Update gitignore
    
    * Add logic for netsuite features
    
    * Update customer join logic for jobs feature
    
    * Bump version in docs
    
    * Update ci pipeline to trigger on pull request
    
    * fix formatting
    
    * Update logic
    
    * fix indentation
    
    * Update ci logic
    
    * Update status badge

--- models/netsuite__time_entry_details.sql
+++ models/netsuite__time_entry_details.sql
@@ -24,16 +24,19 @@ items as (
     select * from {{ var('netsuite_items') }}
 ),
 
+{% if var('netsuite__using_jobs', false) %}
 jobs as (
     select * from {{ var('netsuite_jobs') }}
 ),
-
-job_resources as (
-    select * from {{ var('netsuite_job_resources') }}
-),
+{% endif %}
 
 locations as (
     select * from {{ var('netsuite_locations') }}
+),
+
+{% if var('netsuite__advanced_jobs_enabled', false) %}
+job_resources as (
+    select * from {{ var('netsuite_job_resources') }}
 ),
 
 project_tasks as (
@@ -43,22 +46,27 @@ project_tasks as (
 project_task_assignees as (
     select * from {{ var('netsuite_project_task_assignees') }}
 ),
+{% endif %}
 
 subsidiaries as (
     select * from {{ var('netsuite_subsidiaries') }}
 ),
 
+{% if var('netsuite__time_off_management_enabled', false) %}
 time_off_types as (
     select * from {{ var('netsuite_time_off_types') }}
 ),
+{% endif %}
 
 time_entry_details as (
 
     select
         time_entries.time_entry_id,
         time_entries.memo as time_entry_memo,
+        {% if var('netsuite__time_off_management_enabled', false) %}
         time_entries.time_off_type_id,
         time_off_types.time_off_type_name,
+        {% endif %}
         time_entries.is_time_off,
         time_entries.is_billable,
         time_entries.billable_label,
@@ -70,7 +78,7 @@ time_entry_details as (
         time_entries.time_type,
         time_entries.time_type_full_name
 
-        --The below script allows for accounts table pass through columns.
+        --The below script allows for time entries table pass through columns.
         {{ fivetran_utils.persist_pass_through_columns(pass_through_variable='time_entries_pass_through_columns', identifier='time_entries', transform='') }},
         
         time_entries.employee_id,
@@ -78,51 +86,75 @@ time_entry_details as (
         employees.employee_job_title,
         employees.employee_labor_cost
         
-        --The below script allows for accounts table pass through columns.
+        --The below script allows for employees table pass through columns.
         {{ fivetran_utils.persist_pass_through_columns(pass_through_variable='employees_pass_through_columns', identifier='employees', transform='') }},
         
         time_entries.customer_id as customer_job_id,
         customers.customer_id,
-        coalesce(customers.company_name, time_off_types.time_off_type_name) as company_name,
+        customers.company_name, 
         customers.customer_city,
         customers.customer_state,
         customers.customer_zip_code,
         customers.customer_country,
         customers.customer_external_id
         
-        --The below script allows for accounts table pass through columns.
+        --The below script allows for customers table pass through columns.
         {{ fivetran_utils.persist_pass_through_columns(pass_through_variable='customers_pass_through_columns', identifier='customers', transform='') }},
         
+        {% if var('netsuite__using_jobs', false) %}
         jobs.job_id,
         jobs.job_external_id,
-        coalesce(jobs.job_name, time_off_types.time_off_type_name) as job_name,
-        coalesce(jobs.job_full_name, time_off_types.time_off_type_name) as job_alt_name,
+        jobs.job_name,
+        jobs.job_full_name, 
         jobs.job_status_name,
         jobs.job_type_name,
-        jobs.project_manager_name_last_first,
+        jobs.project_manager_name_last_first
+
+        --The below script allows for jobs table pass through columns.
+        {{ fivetran_utils.persist_pass_through_columns(pass_through_variable='jobs_pass_through_columns', identifier='jobs', transform='') }},
+        {% endif %}
+
+        {% if var('netsuite__advanced_jobs_enabled', false) %}
         job_resources.job_resource_role,
+        project_tasks.project_task_id,
+        project_tasks.project_task_name, 
+        project_tasks.project_task_full_name, 
+        project_task_assignees.unit_price as project_task_unit_price,
+        project_task_assignees.unit_cost as project_task_unit_cost,
+        {% endif %}
+
         coalesce(time_entries.class_id, items.class_id) as class_id,
-        classes.class_full_name,
+        classes.class_full_name
+
+        --The below script allows for jobs table pass through columns.
+        {{ fivetran_utils.persist_pass_through_columns(pass_through_variable='classes_pass_through_columns', identifier='classes', transform='') }},
+
         time_entries.item_id,
         items.item_name,
-        items.item_type_name,
+        items.item_type_name
+
+        --The below script allows for items table pass through columns.
+        {{ fivetran_utils.persist_pass_through_columns(pass_through_variable='items_pass_through_columns', identifier='items', transform='') }},
+
         time_entries.department_id,
         departments.department_name
         
-        --The below script allows for accounts table pass through columns.
+        --The below script allows for departments table pass through columns.
         {{ fivetran_utils.persist_pass_through_columns(pass_through_variable='departments_pass_through_columns', identifier='departments', transform='') }},
         
         time_entries.subsidiary_id,
-        subsidiaries.subsidiary_name,
+        subsidiaries.subsidiary_name
+
+        -- The below script allows for subsidiaries table pass through columns.
+        {{ fivetran_utils.persist_pass_through_columns(pass_through_variable='subsidiaries_pass_through_columns', identifier='subsidiaries', transform='') }},
+
         time_entries.location_id,
         locations.location_name,
         locations.location_city,
-        locations.location_country,
-        project_tasks.project_task_id,
-        coalesce(project_tasks.project_task_name, time_off_types.time_off_type_name) as project_task_name,
-        coalesce(project_tasks.project_task_full_name, time_off_types.time_off_type_name) as project_task_full_name,
-        project_task_assignees.unit_price as project_task_unit_price,
-        project_task_assignees.unit_cost as project_task_unit_cost
+        locations.location_country
+
+        --The below script allows for locations table pass through columns.
+        {{ fivetran_utils.persist_pass_through_columns(pass_through_variable='locations_pass_through_columns', identifier='locations', transform='') }}
     
     from time_entries
     
@@ -132,11 +164,16 @@ time_entry_details as (
     left join classes 
         on classes.class_id = coalesce(time_entries.class_id, items.class_id)
         
+    {% if var('netsuite__using_jobs', false) %}
     left join jobs 
         on jobs.job_id = time_entries.customer_id
         
     left join customers 
         on customers.customer_id = coalesce(jobs.customer_id, time_entries.customer_id)
+    {% else %}
+    left join customers 
+        on customers.customer_id = time_entries.customer_id
+    {% endif %}
         
     left join departments 
         on departments.department_id = time_entries.department_id
@@ -144,12 +181,13 @@ time_entry_details as (
     left join employees 
         on employees.employee_id = time_entries.employee_id
         
+    left join locations 
+        on locations.location_id = time_entries.location_id
+
+    {% if var('netsuite__advanced_jobs_enabled', false) %}
     left join job_resources 
         on job_resources.project_id = time_entries.customer_id
         and job_resources.job_resource_id = time_entries.employee_id
-        
-    left join locations 
-        on locations.location_id = time_entries.location_id
         
     left join project_tasks 
         on project_tasks.project_task_id = time_entries.case_task_event_id
@@ -158,12 +196,15 @@ time_entry_details as (
         on project_task_assignees.project_task_id = project_tasks.project_task_id
         and project_task_assignees.resource_id = employees.employee_id
         and project_task_assignees.service_item_id = items.item_id
+    {% endif %}
         
     left join subsidiaries 
         on subsidiaries.subsidiary_id = time_entries.subsidiary_id
         
-    left join time_off_types 
+    {% if var('netsuite__time_off_management_enabled', false) %}
+    left join time_off_types
         on time_off_types.time_off_type_id = time_entries.time_off_type_id
+    {% endif %}
         
 )
 select * from time_entry_details
